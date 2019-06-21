9371154a503c9fca90c8245b16afeed73794c574
Philipp Schlegel <observing@web.de>
2019-06-21 16:32:45 +0100

    bump version 0.1.2->0.1.3

--- docs/conf.py
+++ docs/conf.py
@@ -27,9 +27,9 @@ copyright = '2019, Philipp Schlegel'
 author = 'Philipp Schlegel'
 
 # The short X.Y version
-version = '0.1.2'
+version = '0.1.3'
 # The full version, including alpha/beta/rc tags
-release = '0.1.2'
+release = '0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
