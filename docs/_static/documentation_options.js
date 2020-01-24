ed1312bc56ca7849014694ad135dbc25b2b736af
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-24 23:09:18 +0000

    Update docs after building Travis build 34 of connectome-neuprint/neuprint-python
    The docs were built from the branch 'master' against the commit
    c101cfa20fb5ebffb364961c02d8908bd8a8ffb6.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/connectome-neuprint/neuprint-python/jobs/279942202.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy --no-require-master --built-docs docs/build/html docs

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2+12.g6dd7942',
+    VERSION: '0.2.1+1.gc101cfa',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
