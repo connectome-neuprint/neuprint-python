da32ccc15fa102d33150447ff96402f82a40a99c
Stuart Berg <bergs@janelia.hhmi.org>
2020-01-25 22:56:48 -0500

    Move parse_properties() into deprecated.fetch

--- neuprint/deprecated/fetch.py
+++ neuprint/deprecated/fetch.py
@@ -13,8 +13,22 @@ Note:
 import pandas as pd
 import numpy as np
 
-from ..utils import make_iterable, parse_properties
+from ..utils import make_iterable
 from ..client import inject_client
+
+
+def parse_properties(props, placeholder):
+    """ Parses list of properties and returns a RETURN string."""
+    props = props if isinstance(props, list) else list(props)
+
+    cypher = []
+    for p in props:
+        if p == 'hasSkeleton':
+            cypher.append(f'exists(({placeholder})-[:Contains]->(:Skeleton)) AS hasSkeleton')
+        else:
+            cypher.append(f'{placeholder}.{p} AS {p}')
+
+    return ','.join(cypher)
 
 
 @inject_client
--- neuprint/utils.py
+++ neuprint/utils.py
@@ -38,20 +38,6 @@ def make_args_iterable(argnames):
     return decorator
 
 
-def parse_properties(props, placeholder):
-    """ Parses list of properties and returns a RETURN string."""
-    props = props if isinstance(props, list) else list(props)
-
-    cypher = []
-    for p in props:
-        if p == 'hasSkeleton':
-            cypher.append(f'exists(({placeholder})-[:Contains]->(:Skeleton)) AS hasSkeleton')
-        else:
-            cypher.append(f'{placeholder}.{p} AS {p}')
-
-    return ','.join(cypher)
-
-
 def where_expr(field, values, regex=False, name='n'):
     """
     Return an expression to match a particular
@@ -74,4 +60,5 @@ def where_expr(field, values, regex=False, name='n'):
         return f"{name}.{field} = '{values[0]}'"
 
     return f"{name}.{field} = {values[0]}"
+
 
