86b32b5a5cbcb6097840c49f89ec8a146f3ec2f5
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-01-29 05:08:36 +0000

    Update docs after building Travis build 52 of connectome-neuprint/neuprint-python
    The docs were built from the branch 'criteria' against the commit
    d946ee86e5d0c3f1874ac4e1117fb2c08750513c.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/connectome-neuprint/neuprint-python/jobs/281341248.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy --no-require-master --built-docs docs/build/html docs-criteria

--- docs-criteria/_static/documentation_options.js
+++ docs-criteria/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.2.3+7.g2fdccba',
+    VERSION: '0.2.3+7.gd946ee8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
