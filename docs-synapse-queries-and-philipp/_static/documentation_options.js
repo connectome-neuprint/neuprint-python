e0402f1cf2314abe61d2e43f5cb7727c53c676c6
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-02-07 18:24:43 +0000

    Update docs after building Travis build 81 of connectome-neuprint/neuprint-python
    The docs were built from the branch 'synapse-queries-and-philipp' against the commit
    34bbf99a284f5f1f0ae248535a3c7725dbe0a0ef.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/connectome-neuprint/neuprint-python/jobs/284943897.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy --no-require-master --built-docs docs/build/html docs-synapse-queries-and-philipp

--- docs-synapse-queries-and-philipp/_static/documentation_options.js
+++ docs-synapse-queries-and-philipp/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.3.4+12.gaaf6aa0',
+    VERSION: '0.3.4+16.g34bbf99',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
