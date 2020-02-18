2d617fb4d724fb0fb538bb81e89d7c0276d70334
Stuart Berg <bergs@janelia.hhmi.org>
2020-02-18 14:27:40 -0500

    setup.py: Python 3.5 not supported.  Remove classifier.

--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(
     install_requires=requirements,
     keywords='neuprint-python',
     classifiers=[
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
