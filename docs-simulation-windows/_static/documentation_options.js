594d4e9d5d01dbc5c9afb0d89423ba2ebb8be5b3
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-05-19 22:06:33 +0000

    Update docs after building Travis build 137 of connectome-neuprint/neuprint-python
    The docs were built from the branch 'simulation-windows' against the commit
    c1d7e2aea5db516052efcab0842a769cd76306d9.
    
    The Travis build that generated this commit is at
    https://travis-ci.com/connectome-neuprint/neuprint-python/jobs/337254237.
    
    The doctr command that was run is
    
        /home/travis/miniconda/bin/doctr deploy --no-require-master --built-docs docs/build/html docs-simulation-windows

--- docs-simulation-windows/_static/documentation_options.js
+++ docs-simulation-windows/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.9+1.g68c3181',
+    VERSION: '0.4.9+1.gc1d7e2a',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
