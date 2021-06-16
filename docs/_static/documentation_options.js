2d7e265704704687067fbd39c15a6fc2ce871ff1
Stuart Berg <bergs@janelia.hhmi.org>
2021-06-16 12:36:49 -0400

    Updated docs for 0.4.15

--- docs/_static/documentation_options.js
+++ docs/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.4.14',
+    VERSION: '0.4.14+10.gc5c01a0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
