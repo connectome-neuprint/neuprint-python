connectome-neuprint/neuprint-python

commit 58090924b4f9b4cbda21a642c54b6d84efc29e57

Avoid empty dataframes in pd.concat(), since they can corrupt the dtype of the result

neuprint/queries/connectivity.py
@@ -461,7 +461,9 @@ def fetch_adjacencies(sources=None, targets=None, rois=None, min_roi_weight=1, m
                            e.weight as weight,
                            e.roiInfo as roiInfo
                 """
-                conn_tables.append(client.fetch_custom(q))
+                t = client.fetch_custom(q)
+                if len(t) > 0:
+                    conn_tables.append(t)
         else:
             # Break targets into batches
             for batch_start in trange(0, len(targets_df), batch_size):
@@ -494,7 +496,9 @@ def fetch_adjacencies(sources=None, targets=None, rois=None, min_roi_weight=1, m
                            e.weight as weight,
                            e.roiInfo as roiInfo
                 """
-                conn_tables.append(client.fetch_custom(q))
+                t = client.fetch_custom(q)
+                if len(t) > 0:
+                    conn_tables.append(t)
 
         # Combine batches
         connections_df = pd.concat(conn_tables, ignore_index=True)

neuprint/queries/mito.py
@@ -97,7 +97,8 @@ def fetch_mitochondria(neuron_criteria, mito_criteria=None, batch_size=10, *, cl
         batch_criteria = copy.copy(neuron_criteria)
         batch_criteria.bodyId = batch_bodies
         batch_df = _fetch_mitos(batch_criteria, mito_criteria, client)
-        batch_dfs.append( batch_df )
+        if len(batch_df) > 0:
+            batch_dfs.append( batch_df )
 
     if batch_dfs:
         return pd.concat( batch_dfs, ignore_index=True )
@@ -273,7 +274,8 @@ def fetch_synapses_and_closest_mitochondria(neuron_criteria, synapse_criteria=No
         batch_criteria = copy.copy(neuron_criteria)
         batch_criteria.bodyId = batch_bodies
         batch_df = _fetch_synapses_and_closest_mitochondria(batch_criteria, synapse_criteria, client)
-        batch_dfs.append( batch_df )
+        if len(batch_df) > 0:
+            batch_dfs.append( batch_df )
 
     if batch_dfs:
         return pd.concat( batch_dfs, ignore_index=True )

neuprint/queries/recon.py
@@ -50,7 +50,9 @@ def fetch_output_completeness(criteria, complete_statuses=['Traced'], batch_size
     batch_results = []
     for start in trange(0, len(bodies), batch_size):
         criteria.bodyId = bodies[start:start+batch_size]
-        batch_results.append( _fetch_output_completeness(criteria, complete_statuses, client) )
+        _df = _fetch_output_completeness(criteria, complete_statuses, client)
+        if len(_df) > 0:
+            batch_results.append( _df )
     return pd.concat( batch_results, ignore_index=True )
 
 

neuprint/queries/synapses.py
@@ -113,7 +113,8 @@ def fetch_synapses(neuron_criteria, synapse_criteria=None, batch_size=10, *, cli
         batch_criteria = copy.copy(neuron_criteria)
         batch_criteria.bodyId = batch_bodies
         batch_df = _fetch_synapses(batch_criteria, synapse_criteria, client)
-        batch_dfs.append( batch_df )
+        if len(batch_df) > 0:
+            batch_dfs.append( batch_df )
 
     if batch_dfs:
         return pd.concat( batch_dfs, ignore_index=True )
@@ -279,7 +280,9 @@ def fetch_mean_synapses(neuron_criteria, synapse_criteria=None, batch_size=100, 
             batch_df = _fetch_mean_synapses_per_roi(batch_criteria, synapse_criteria, client)
         else:
             batch_df = _fetch_mean_synapses_per_whole_neuron(batch_criteria, synapse_criteria, client)
-        batch_dfs.append( batch_df )
+
+        if len(batch_df) > 0:
+            batch_dfs.append( batch_df )
 
     if batch_dfs:
         return pd.concat( batch_dfs, ignore_index=True )
@@ -623,7 +626,8 @@ def fetch_synapse_connections(source_criteria=None, target_criteria=None, synaps
                                                            synapse_criteria,
                                                            min_total_weight,
                                                            client )
-                syn_dfs.append(batch_syn_df)
+                if len(batch_syn_df) > 0:
+                    syn_dfs.append(batch_syn_df)
                 progress.update(len(batch_syn_df))
 
     syn_df = pd.concat(syn_dfs, ignore_index=True)

commit c275dff1645a05bc3bbd1c906e886d2989c19e7b

fetch_adjacencies: Fix edge case when no adjencies were found. Fixes bug introduced in 58090924b4f9b4cbda21a642c54b6d84efc29e57

neuprint/queries/connectivity.py
@@ -500,6 +500,9 @@ def fetch_adjacencies(sources=None, targets=None, rois=None, min_roi_weight=1, m
                 if len(t) > 0:
                     conn_tables.append(t)
 
+        if not conn_tables:
+            return []
+
         # Combine batches
         connections_df = pd.concat(conn_tables, ignore_index=True)
         return connections_df